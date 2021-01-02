127ddd07c1c9e76a7172df49a6e8c7d3ca92d2ea
Enes <enessolak99@gmail.com>
2021-01-02 20:26:52 +0300

    fix admin authentication and registration

--- controllers/Admin/auth.py
+++ controllers/Admin/auth.py
@@ -3,19 +3,26 @@ from flask_restful import fields, marshal_with, marshal, inputs
 from models.admins import Admin
 from models.appointments import Appointment
 from models.fixtures import Fixture
-from utils.jwt import authenticated
+from utils.password import check_encrypted_password, encrypt_password
+from utils.jwt import authenticated, encode_jwt
 from app import db
 
-admin_post_parser = reqparse.RequestParser()
-admin_post_parser.add_argument(
-    'first_name', type=inputs.regex('^\w{,20}$'), required=True, location=['json'], help='first_name parameter is required')
-admin_post_parser.add_argument(
-    'last_name', type=inputs.regex('^\w{,20}$'), required=True, location=['json'], help='last_name parameter is required')
-admin_post_parser.add_argument(
+login_post_parser = reqparse.RequestParser()
+login_post_parser.add_argument(
     'password', type=inputs.regex('^\w{6,16}$'), required=True, location=['json'], help='password parameter is required')
-admin_post_parser.add_argument(
+login_post_parser.add_argument(
+    'email', type=inputs.regex('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'), required=True, location=['json'], help='email parameter is required')
+
+register_post_parser = reqparse.RequestParser()
+register_post_parser.add_argument(
+    'first_name', type=inputs.regex('^\w{,20}$'), required=True, location=['json'], help='first_name parameter is required')
+register_post_parser.add_argument(
+    'last_name', type=inputs.regex('^\w{,20}$'), required=True, location=['json'], help='last_name parameter is required')
+register_post_parser.add_argument(
+    'password', type=inputs.regex('^\w{6,16}$'), required=True, location=['json'], help='password parameter is required')
+register_post_parser.add_argument(
     'email', type=inputs.regex('^\w{,100}$'), required=True, location=['json'], help='email parameter is required')
-admin_post_parser.add_argument(
+register_post_parser.add_argument(
     'tc_number', type=inputs.regex('^[0-9]{11}$'), required=True, location=['json'], help='tc_number parameter is required')
 
 class AdminAuthLoginResource(Resource):
@@ -30,16 +37,15 @@ class AdminAuthLoginResource(Resource):
             if check_encrypted_password(password, admin.password):
                 return {
                     'status': 'success',
-                    'access_token': encode_jwt(admin.id)
+                    'access_token': encode_jwt(admin_id=admin.id)
                 }
         except:
             return {
                 'status': 'error',
-                'message': 'User not found with provided credentials'
+                'message': 'Admin not found with provided credentials'
             }, 404
 
 class AdminAuthRegisterResource(Resource):
-    @marshal_with(register_fields)
     def post(self):
         args = register_post_parser.parse_args()
 
